459bbc68ec6508c3522a0ca3c114364db215aa4b
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-07-21 16:05:39 +0500

    Basic Project Folder Structure setup and dependencies for backend installed

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+const metadata = {
+  title: "promptopia",
+  description: "Discove and Share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
